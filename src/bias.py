7f82c1042a7980cff9078d1bc8dcd9f572bd5098
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2018-05-11 11:06:52 -0700

    Add passing test to ensure can tokenize wordlist.

--- src/bias.py
+++ src/bias.py
@@ -6,3 +6,13 @@ def test_can_read_examples():
     for file in examples.values():
         with open(file, 'r') as stream:
             assert stream.readable()
+
+from nltk import word_tokenize
+import nltk
+nltk.download('punkt')
+
+def test_can_tokenize_into_wordlist():
+    with open(examples['m'], 'r') as stream:
+        words = word_tokenize(stream.read())
+        assert isinstance(words, list)
+        assert len(words) > 0
