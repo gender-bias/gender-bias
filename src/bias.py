4e36d25504e6774273e96b980ff47c0c5aa69c45
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2018-05-11 12:10:06 -0700

    Add test of lemmatizing - not working yet.

--- src/bias.py
+++ src/bias.py
@@ -10,6 +10,7 @@ def test_can_read_examples():
 from nltk import word_tokenize
 import nltk
 nltk.download('punkt')
+nltk.download('wordnet')
 
 def test_can_tokenize_into_wordlist():
     with open(examples['m'], 'r') as stream:
@@ -23,3 +24,11 @@ def test_stemming():
     words = ['strangely']
     stemmed = [porter.stem(w) for w in words]
     assert stemmed == ['strang']
+
+wnl = nltk.WordNetLemmatizer()
+
+def test_lemmatizing():
+    words = ['strangely']
+    stemmed = [porter.stem(w) for w in words]
+    lemmaed = [wnl.lemmatize(w) for w in stemmed]
+    #assert lemmaed == ['strange']
