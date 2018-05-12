7a82da61bde3245f191086f65e20aa5441e8ab2c
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2018-05-11 23:41:53 -0700

    Improve names of Document member functions.

--- src/gender_bias.py
+++ src/gender_bias.py
@@ -12,22 +12,22 @@ class Document:
         with open(filename, 'r') as f:
             self._text = f.read()
 
-    def sentences_from_text(self):
+    def sentences(self):
         return [s.replace('\n',' ') for s in nltk.sent_tokenize(self._text)]
 
-    def words_from_text(self):
+    def words(self):
         return nltk.word_tokenize(self._text)
 
-    def categorize(self):
-        words = self.words_from_text()
+    def words_by_part_of_speech(self):
+        words = self.words()
         tagged = nltk.pos_tag(words)
         categories = {}
         for type in {t[1] for t in tagged}:
             categories[type] = [t[0] for t in tagged if t[1] == type]
         return categories
 
-    def stem(self):
-        words = self.words_from_text()
+    def stemmed_words(self):
+        words = self.words()
         return [porter.stem(w) for w in words]
 
 ##########
@@ -42,9 +42,9 @@ def test_can_read_examples(example_doc):
     with open(example_doc[0], 'r') as stream:
         assert stream.readable()
 
-def test_words_from_text(example_doc):
+def test_words(example_doc):
     t = Document(example_doc[0])
-    words = t.words_from_text()
+    words = t.words()
     assert isinstance(words, list)
     assert len(words) > 0
 
@@ -52,8 +52,8 @@ porter = nltk.PorterStemmer()
 
 def test_stemming(example_doc):
     t = Document(example_doc[0])
-    stemmed = t.stem()
-    assert sum([len(x) for x in t.words_from_text()]) > sum([len(x) for x in stemmed])
+    stemmed = t.stemmed_words()
+    assert sum([len(x) for x in t.words()]) > sum([len(x) for x in stemmed])
 
 wnl = nltk.WordNetLemmatizer()
 
@@ -65,12 +65,12 @@ def test_lemmatizing():
 
 def test_sentence(example_doc):
     t = Document(example_doc[0])
-    s = t.sentences_from_text()
+    s = t.sentences()
     for ss in s:
         assert "\n" not in ss
     assert len(s) == example_doc[1]
 
-def test_categorization(example_doc):
+def test_words_by_part_of_speech(example_doc):
     t = Document(example_doc[0])
-    c = t.categorize()
+    c = t.words_by_part_of_speech()
     assert len(c[',']) == example_doc[2]
