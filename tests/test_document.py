cd7825dfdbae0bf25e58070712648fe1f696410f
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2018-05-20 09:32:42 -0700

    Document tests: Add test for Document constructor equality.
    Also make examples dict simpler & remove out of date comment.

--- tests/test_document.py
+++ tests/test_document.py
@@ -11,19 +11,31 @@ wnl = nltk.WordNetLemmatizer()
 
 example_dir = os.path.dirname(__file__) + "/../example_letters/"
 
-# filename, assertions = number-of-sentences, commas
-examples = dict(m=dict(file=example_dir + "letterofRecM", sentences=13, commas=12, words=446),
-                f=dict(file=example_dir + "letterofRecW", sentences=26, commas=29, words=693))
-
+examples = {'m': dict(file=example_dir + "letterofRecM", sentences=13, commas=12, words=446),
+            'f': dict(file=example_dir + "letterofRecW", sentences=26, commas=29, words=693)
+}
 
 @fixture(params=examples.values())
 def example_doc(request):
-    return dict(request.param, document=Document(request.param['file']))
+    with open(request.param['file']) as stream:
+        return dict(request.param,
+                    document=Document(request.param['file']),
+                    document_string=Document(stream.read()))
 
 
 def test_can_read_examples(example_doc):
     with open(example_doc['file'], 'r') as stream:
         assert stream.readable()
+
+
+def test_document_from_file_equals_document_from_string(example_doc):
+    file = example_doc['document']
+    string = example_doc['document_string']
+    assert file.sentences() == string.sentences()
+    assert file.words() == string.words()
+    assert file.words_with_indices() == string.words_with_indices()
+    assert file.words_with_indices() == string.words_with_indices()
+    assert file.stemmed_words() == string.stemmed_words()
 
 
 def test_words(example_doc):
