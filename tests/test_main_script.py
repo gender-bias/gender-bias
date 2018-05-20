3cae69f01285a7a8544a4bd525398e355f4bd551
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2018-05-20 10:18:30 -0700

    Script tests: Simplify by extracting Popen code into helper function.

--- tests/test_main_script.py
+++ tests/test_main_script.py
@@ -8,15 +8,16 @@ examples = ["example_letters/letterofRecM", "example_letters/letterofRecW"]
 def example(request):
     return request.param
 
+def output_with_options(options):
+    popen = Popen(script + options, stdout=PIPE, universal_newlines=True)
+    return popen.communicate()[0]
+
 def test_script_help():
-    popen = Popen(script + ["-h"], stdout=PIPE, universal_newlines=True)
-    output = popen.communicate()[0]
     help_text = 'usage: genderbias [-h] [--file FILE]\n\nCLI for gender-bias detection\n\noptional arguments:\n  -h, --help            show this help message and exit\n  --file FILE, -f FILE  The file to check\n'
-    assert output == help_text
+    assert output_with_options(["-h"]) == help_text
 
 def test_script_file_flags(example):
-    popen = Popen(script + ["-f", example], stdout=PIPE, universal_newlines=True)
-    output = popen.communicate()[0]
+    output = output_with_options(["-f", example])
     for line in output.split("\n"):
         if line:
             assert line[0] == "["
