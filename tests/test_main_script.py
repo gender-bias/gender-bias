e87f5d8a93c60f247f6e6152003b12a055ef238b
neiljp (Neil Pilgrim) <github@kepier.clara.net>
2018-05-20 21:11:01 -0700

    genderbias: Add json output feature, using Report.to_dict().
    Also:
    * add related script tests
    * amend the help test to be less specific, so it doesn't break so often

--- tests/test_main_script.py
+++ tests/test_main_script.py
@@ -1,5 +1,6 @@
 from subprocess import Popen, PIPE
 import os
+import json
 from pytest import fixture
 
 script = ["genderbias"]
@@ -14,6 +15,11 @@ def example(request):
 def file_flag(request):
     return request.param
 
+@fixture(params = ["--json", "-j"])
+def json_flag(request):
+    return request.param
+
+
 def output_with_options(options, feed_in=""):
     popen = Popen(script + options, stdin=PIPE, stdout=PIPE, universal_newlines=True)
     return popen.communicate(feed_in)[0]
@@ -23,8 +29,8 @@ def test_meta_ensure_examples_exist(example):
     assert os.path.isfile(example['file'])
 
 def test_script_help():
-    help_text = 'usage: genderbias [-h] [--file FILE]\n\nCLI for gender-bias detection\n\noptional arguments:\n  -h, --help            show this help message and exit\n  --file FILE, -f FILE  The file to check\n'
-    assert output_with_options(["-h"]) == help_text
+    start_help_text = 'usage: genderbias [-h]'
+    assert start_help_text in output_with_options(["-h"])
 
 def test_script_file_flags(example, file_flag):
     output = output_with_options([file_flag, example['file']])
@@ -35,3 +41,15 @@ def test_script_input_from_stdin(example):
         text = stream.read()
     output = output_with_options([], feed_in=text)
     assert len(output.strip().split("\n")) == example['output_lines']
+
+def test_script_json_output(example, file_flag, json_flag):
+    output = output_with_options([file_flag, example['file'], json_flag])
+    data = json.loads(output.strip())
+    assert isinstance(data, list)
+    for element in data:
+        assert isinstance(element, dict)
+        for key in ('name', 'summary', 'flags'):
+            assert key in element
+        assert isinstance(element['name'], str)
+        assert isinstance(element['summary'], str)
+        assert isinstance(element['flags'], list)
