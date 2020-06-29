9b9c846c66eb9cc2c1cc0c3c22ae1e2e6a2a0c1f
Jordan Matelsky <j6k4m8@gmail.com>
2020-06-29 13:10:55 -0400

    remove old code (check git history for more info)

--- genderbias/detector.py
+++ genderbias/detector.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod
 
+from .document import Document
+
 
 class BiasBoundsException(Exception):
     pass
--- public_html/cgi-bin/test.py
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/python
-
-print "Content-type: text/html\n\n"
-print "<html>Hello world!</html>"
